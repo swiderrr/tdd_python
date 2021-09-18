05111c893fcbc706845d0cb0296f553236b7ffd5
Karol <swidrak.karol@gmail.com>
2021-09-18 22:17:07 +0200

    Pierwszy test jednostkowy, mapowanie url i atrapa widoku.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,45 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-assert 'Django' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    def tearDown(self):
+        self.browser.quit()
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edyta dowiedziała sie o nowej, wspaniałej aplikacji w postaci listy rzeczy do zrobienia
+        # Postanowiła więc przejść na stronę główną tej aplikacji
+        self.browser.get('http://localhost:8000')
+
+        #Zwróciła uwagę, że tytuł strony i nagłówek zawierają słowo Listy.
+        self.assertIn('Listy', self.browser.title)
+        self.fail('Zakończenie testu')
+
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+#Od razu zostaje zachęcona, aby wpisać rzecz do zrobienia.
+
+#W polu tekstowym wpisała "Kupić pawie pióra" (hobby Edyty
+#polega na tworzeniu ozdobnych przynęt).
+
+#Po naciśnięciu klawisza Enter strona została uaktulaniona i wyświetla
+#"1: Kupić pawie pióra" jako element listy do zrobienia.
+
+#Na stronie nadal znajduje się pole tekstowe zachęcające do podania kolejnego zadaniai.
+#Edyta wpisała "Użyć pawich piór do zrobienia przynęty" (Edyta jest niezwykle skrupulatna).
+
+# Strona została ponownie uaktulaniona i teraz wyświetla dwa elementy na liście rzeczy do zrobienia.
+
+#Edyta była ciekawa, czy witryna zapamięta jej listę. Zwróciła uwagę na wygenerowany dla niej
+#unikatowy adres URL,obok którego znajduje się pewien tekst z wyjaśnieniem
+
+#Przechodzi pod podany adres URL i widzi wyświetloną swoją listę rzeczy do zrobienia.
+
+#Usatysfakcjonowana kładzie się spać.
+
+browser.quit()
