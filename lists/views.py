c1eff272dac8437d4d29643dbca32a5f5545cd3a
Karol <swidrak.karol@gmail.com>
2021-09-19 13:08:08 +0200

    Refaktoryzacja widoku strony glownej w celu uzycia szablonu.

--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><title>Listy rzeczy do zrobienia</title></html>')
+    return render(request, 'home.html')
